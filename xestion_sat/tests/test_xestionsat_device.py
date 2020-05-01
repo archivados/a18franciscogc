73165e54d2ab5ee5b01d5b4fa9fbc4a2de53cd3e
Fco. Javier González Campos <a18franciscogc@iessanclemente.net>
2020-05-01 17:38:51 +0200

    First test approach of the xestionsat_device model

---
+++ xestion_sat/tests/test_xestionsat_device.py
@@ -0,0 +1,36 @@
+from .test_common import TestCommonData
+
+
+class XestionsatTestDevice(TestCommonData):
+    '''Test.
+    '''
+
+    def setUp(self):
+        super(XestionsatTestDevice, self).setUp()
+        self.DeviceModel = self.env['xestionsat.device']
+
+    def test_create_device(self):
+        '''Test.
+        '''
+
+        # Device 1
+        self.device_1 = self.DeviceModel.create(
+            {
+                # Required fields
+                # 'created_by_id': self.test_admin_1.id,
+                'owner_id': self.partner_1.id,
+                'headquarter_id': self.partner_1_address_2.id,
+                'name': 'Equipo 1',
+                # 'state': 'operational',
+
+                # Optional fields
+                # 'user_ids': self.partner_1_employee_1,
+                # 'devicecomponents_ids': 'xestionsat.device.component',
+                # 'internal_id': '',
+                # 'location': '',
+                # 'description': '',
+                # 'observation': '',
+                # 'date_registration': '',
+                # 'date_cancellation': '',
+            }
+        )
