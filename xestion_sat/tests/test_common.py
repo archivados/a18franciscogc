21abc75efd1cb6b6eb0eb9791fb734509bd54095
Fco. Javier González Campos <a18franciscogc@iessanclemente.net>
2020-05-02 15:04:11 +0200

    test_common: Added comments

--- xestion_sat/tests/test_common.py
+++ xestion_sat/tests/test_common.py
@@ -1,8 +1,19 @@
+# 1: imports of python lib
+
+# 2: import of known third party lib
+
+# 3:  imports of odoo
 from odoo.tests.common import TransactionCase
+
+# 4:  imports from odoo modules
+
+# 5: local imports
+
+# 6: Import of unknown third party lib
 
 
 class TestCommonData(TransactionCase):
-    '''Test.
+    '''Common data for testing.
     '''
 
     def setUp(self):
