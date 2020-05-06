a5a1851be49813c4dc39b168a13c66fa30726b60
Fco. Javier González Campos <a18franciscogc@iessanclemente.net>
2020-05-06 22:11:20 +0200

    test_xestionsat: added test incidence (1st approximation)

--- xestion_sat/tests/test_xestionsat.py
+++ xestion_sat/tests/test_xestionsat.py
@@ -22,6 +22,9 @@ class XestionsatTest(TestCommonData):
         super(XestionsatTest, self).setUp()
         self.Device = self.env['xestionsat.device']
         self.DeviceComponent = self.env['xestionsat.device.component']
+        self.Incidence = self.env['xestionsat.incidence']
+        self.IncidenceState = self.env['xestionsat.incidence.state']
+        self.IncidencePlace = self.env['xestionsat.incidence.assistance_place']
 
         # Create Devices Components
         # Device Component 1 (Product)
@@ -36,7 +39,6 @@ class XestionsatTest(TestCommonData):
                 # 'date_cancellation': '',
             }
         )
-
         # Device Component 2 (Product)
         self.componet_2 = self.DeviceComponent.create(
             {
@@ -49,7 +51,6 @@ class XestionsatTest(TestCommonData):
                 # 'date_cancellation': '',
             }
         )
-
         # Device Component 3 (Product)
         self.componet_3 = self.DeviceComponent.create(
             {
@@ -62,7 +63,6 @@ class XestionsatTest(TestCommonData):
                 # 'date_cancellation': '',
             }
         )
-
         # Device Component 4 (Product)
         self.componet_4 = self.DeviceComponent.create(
             {
@@ -73,6 +73,46 @@ class XestionsatTest(TestCommonData):
                 'observation': 'Unha observación',
                 'date_registration': datetime.now().strftime('%Y-%m-%d'),
                 # 'date_cancellation': '',
+            }
+        )
+
+        # Create Incidence States
+        # Incidence State 1
+        self.incidence_state_1 = self.IncidenceState.create(
+            {
+                # Required fields
+                'state': 'Pending',
+                'sequence': 1,
+                # Optional fields
+                'description': 'Work without starting',
+            }
+        )
+        # Incidence State 2
+        self.incidence_state_2 = self.IncidenceState.create(
+            {
+                # Required fields
+                'state': 'Started',
+                'sequence': 2,
+                # Optional fields
+                'description': 'Work in progress',
+            }
+        )
+
+        # Create Incidence Assistance Place
+        # Incidence Assistance Place 1
+        self.incidence_place_1 = self.IncidencePlace.create(
+            {
+                # Required fields
+                'assistance_place': 'In workshop',
+                'description': 'In our workshop',
+            }
+        )
+        # Incidence Assistance Place 2
+        self.incidence_place_2 = self.IncidencePlace.create(
+            {
+                # Required fields
+                'assistance_place': 'On-site',
+                'description': 'At the Customers workplace',
             }
         )
 
@@ -178,14 +218,37 @@ class XestionsatTest(TestCommonData):
         with self.assertRaises(ValidationError):
             self.device_1['user_ids'] = (4, self.partner_2_employee_2.id)
 
+        return self.device_1
+
     def test_create_incidence(self):
         """Incidence model test.
         """
-        self.device_1
-        self.assertEqual(
-            1,
-            2,
-            msg='\nRemove Componet ERRO:'
-            + '\n Product: ' + self.device_1.name
-            + '\n len(len_devicecomponents_ids): '
+        device_1 = self.test_create_device()
+
+        # Incidence 1
+        self.incidence_1 = self.Incidence.sudo(self.test_admin_1).create(
+            {
+                # Required fields
+                'created_by_id': self.test_admin_1.id,
+                'customer_id': device_1['owner_id'].id,
+                # 'device_ids': self.partner_1.id,
+                'title': 'Incidencia 1: ' + device_1.name,
+                'failure_description': 'Non se conecta a internet',
+                'state': self.incidence_state_1.id,
+
+                # Optional fields
+                'observation': 'Unha observación',
+                'assistance_place': self.incidence_place_1.id,
+                # 'incidence_action_ids': ,
+                'date_start': datetime.now().strftime('%Y-%m-%d'),
+                # 'date_end': '',
+            }
         )
+
+        # Check that device is created or not
+        assert self.incidence_1, "Device not created"
+
+        # Check constraints
+        # Check Odoo user constraint
+        with self.assertRaises(ValidationError):
+            self.incidence_1.created_by_id = self.test_admin_2
