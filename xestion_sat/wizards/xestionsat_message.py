a2d89aee2256ef39137e9631e0e5a0141654b2ad
Fco. Javier González Campos <a18franciscogc@iessanclemente.net>
2020-06-06 11:16:13 +0200

    Refactored

--- xestion_sat/wizards/xestionsat_message.py
+++ xestion_sat/wizards/xestionsat_message.py
@@ -18,8 +18,3 @@ class Message(models.TransientModel):
     _name = 'xestionsat.message'
 
     message = fields.Text('Message', required=True)
-
-    @api.multi
-    def action_ok(self):
-        """ close wizard"""
-        return {'type': 'ir.actions.act_window_close'}
